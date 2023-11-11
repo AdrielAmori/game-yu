2643a2b639bebc48ff2aefdb10a6f93746b852f6
AdrielAmori <adriel680@gmail.com>
2023-11-11 14:30:33 -0300

    update index.html

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -203,8 +203,12 @@ async function playAudio(status) {
 
 function init() {
     showHiddenCardFieldsImages(false);
+
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 };
 
 init();
